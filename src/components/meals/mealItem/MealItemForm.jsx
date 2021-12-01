9fdc291211f38e046733d094f360db706d6dd966
Gabones <gabaoalb@gmail.com>
2021-12-01 00:17:34 -0300

    antes da modal

--- src/components/meals/mealItem/MealItemForm.jsx
+++ src/components/meals/mealItem/MealItemForm.jsx
@@ -1,5 +1,20 @@
+import Input from '../../ui/Input';
+import classes from './MealItemForm.module.css';
 
+const MealItemForm = (props) => {
+    return (
+        <form className={classes.form} action="">
+            <Input label="Amount" input={{
+                id: 'amount_' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }}/>
+            <button>+ Add</button>
+        </form>
+    )
+};
 
-const MealItem = (props) => {
-
-}
+export default MealItemForm;
