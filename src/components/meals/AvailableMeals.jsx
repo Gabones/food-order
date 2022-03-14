8479b48eff7fa85f724783ed72d14dcabcfc463e
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.net>
2022-03-14 13:25:07 -0300

    modal form

--- src/components/meals/AvailableMeals.jsx
+++ src/components/meals/AvailableMeals.jsx
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
 
     useEffect(() => {
         const fetchMeals = async () => {
-            const response = await fetch('https://reactapp-1cd72-default-rtdb.firebaseio.com/meals');
+            const response = await fetch('https://reactapp-1cd72-default-rtdb.firebaseio.com/meals.json');
 
             if (!response.ok) {
                 throw new Error('Something went wrong!');
